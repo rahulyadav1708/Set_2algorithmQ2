4bb2260eb183a0e0964ed77a5698b45b63ebd6a0
rahulyadav1708 <141802799+rahulyadav1708@users.noreply.github.com>
2024-08-03 14:34:40 +0530

    Add files via upload

---
+++ fibonaccielement/src/fibonaccielement/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fibonaccielement
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Define the array
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8,13 };
+
+            // Find the Fibonacci numbers up to the maximum value in the array
+            HashSet<int> fibonacciNumbers = GenerateFibonacciUpTo(array[array.Length - 1]);
+
+            // Find the output by checking if the array elements are Fibonacci numbers
+            int num = 0;
+            foreach (int number in array)
+            {
+                if (fibonacciNumbers.Contains(number))
+                {
+                    num++;
+                   Console.WriteLine(number);
+                    
+                    Console.ReadKey();
+                }
+               
+
+            }
+            Console.WriteLine("the longest sequence have "  + num + " element");
+            Console.ReadKey();
+        }
+
+        static HashSet<int> GenerateFibonacciUpTo(int max)
+        {
+            HashSet<int> fibonacciNumbers = new HashSet<int>();
+            int a = 0, b = 1;
+            while (a <= max)
+            {
+                fibonacciNumbers.Add(a);
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+            return fibonacciNumbers;
+        }
+       
+
+    }
+   
+}
+
+
+
+
+
